2f2c46d7541f7ba08cd3c87c732e4d5faa7eab05
Thomas Arrow <thomasarrow@gmail.com>
2015-12-04 11:45:29 +0000

    Fixed core query test and added comments to tests

--- test/test_empclib.py
+++ test/test_empclib.py
@@ -1,4 +1,6 @@
 import unittest
+from mock import patch
+import os.path, json
 from .. epmclib.getPMCID import getPMCID
 from .. epmclib.getPMID import getPMID
 from .. epmclib.exceptions import *
@@ -7,16 +9,19 @@ from .. epmclib.exceptions import *
 class EPMClibTester(unittest.TestCase):
 
     def test_getpmcid_resolves(self):
+        "Test resolves returns True for a real PMCID"
         pmcid = getPMCID('PMC288264')
         result = pmcid.resolves()
         self.assertEqual(True, result)
 
     def test_getpmcid_does_not_resolve(self):
+        """Test resolves returns False for a fake pmcid"""
         pmcid = getPMCID('PMC2882643532')
         result = pmcid.resolves()
         self.assertEqual(False, result)
 
     def test_get_pmcid_lite_data(self):
+        """Test lite query against inline json"""
         pmcid = getPMCID('PMC288264')
         pmcid.liteQuery()
         result = pmcid.rawresults
@@ -39,31 +44,16 @@ class EPMClibTester(unittest.TestCase):
         self.assertEqual(idealresult, result)
 
     def test_get_pmcid_core_data(self):
+        """Test the json of a core data query"""
         pmcid = getPMCID('PMC288264')
         pmcid.coreQuery()
         result = pmcid.rawresults
-        idealresult = {'resultList': {'result':
-                                          [{'id': '8376584',
-                                            'title': 'beta 2-Microglobulin modified with advanced glycation end product'
-                                                     's is a major component of hemodialysis-associated amyloidosis.',
-                                            'issue': '3', 'hasReferences': 'Y', 'hasBook': 'N', 'pubYear': '1993',
-                                            'isOpenAccess': 'N', 'source': 'MED', 'citedByCount': 114,
-                                            'pageInfo': '1243-1252', 'doi': '10.1172/jci116696',
-                                            'journalTitle': 'J Clin Invest', 'hasPDF': 'Y', 'hasTextMinedTerms': 'Y',
-                                            'hasDbCrossReferences': 'N', 'journalVolume': '92',
-                                            'journalIssn': '0021-9738', 'luceneScore': 'NaN',
-                                            'hasTMAccessionNumbers': 'N',
-                                            'authorString': 'Miyata T, Oda O, Inagi R, Iida Y, Araki N, Yamada N,'
-                                                            ' Horiuchi S, Taniguchi N, Maeda K, Kinoshita T.',
-                                            'inEPMC': 'Y', 'inPMC': 'N', 'pmcid': 'PMC288264',
-                                            'pubType': 'journal article; research-article', 'pmid': '8376584',
-                                            'hasLabsLinks': 'Y'}]},
-                       'request': {'resultType': 'lite', 'page': 1, 'synonym': False, 'query': 'PMCID:PMC288264',
-                                   'pageSize': 25}, 'hitCount': 1, 'version': '4.4.0'}
-        print(result)
+        testfile = open(os.path.normpath('resources/coretest.json'), 'r')
+        idealresult = json.load(testfile)
         self.assertEqual(idealresult, result)
 
     def test_get_pmcid_title_only(self):
+        """Test the title of a pmcid for known result"""
         pmcid = getPMCID('PMC288264')
         pmcid.getTitle()
         result = pmcid.title
@@ -71,14 +61,17 @@ class EPMClibTester(unittest.TestCase):
                           's is a major component of hemodialysis-associated amyloidosis.', result)
 
     def test_raise_id_not_found_exception(self):
+        """Check IDNotResolvedException is raised for known failer"""
         pmcid = getPMCID('PMC2882643532')
         self.assertRaises(IDNotResolvedException, pmcid.getTitle)
 
     def test_no_prefix_on_pmcid_is_added(self):
+        """Tests PMC prefix is added if PMCid is not given one EuropePMC requires this to be there"""
         pmcid = getPMCID('2882643532')
         self.assertEquals(pmcid.query[6:],'PMC2882643532')
 
     def test_query_string_pmid(self):
+        """Test correct query string is added for PMIDS"""
         pmid = getPMID('1219350')
         self.assertEquals(pmid.query, 'ext_id:1219350 src:med')
 
