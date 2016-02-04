8f499b0fb3894aba707e55f1dd4e4b7ff3a6ca6e
Tom Arrow <bomarrow1@gmail.com>
2016-02-04 10:36:37 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EPMCLib',
+      version='0.1',
+      description='Python client for EuropePMC API',
+      author='Thomas Arrow',
+      author_email='thomasarrow@gmail.com',
+      url='https://github.com/tarrow/epmclib',
+      packages=['epmclib'],
+     )
